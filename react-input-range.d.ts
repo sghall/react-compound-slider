223e8d18f3fcc1f62a7dcf0891e070cb4c2f654c
Steve Hall <hall.steven.g@gmail.com>
2017-08-10 18:45:41 -0700

    add dev files

---
+++ react-input-range.d.ts
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+export interface Range {
+  max: number;
+  min: number;
+}
+
+export interface InputRangeClassNames {
+  activeTrack: string;
+  disabledInputRange: string;
+  inputRange: string;
+  labelContainer: string;
+  maxLabel: string;
+  minLabel: string;
+  slider: string;
+  sliderContainer: string;
+  track: string;
+  valueLabel: string;
+}
+
+export interface InputRangeProps {
+  ariaLabelledby?: string;
+  ariaControls?: string;
+  classNames?: InputRangeClassNames;
+  disabled?: boolean;
+  draggableTrack?: boolean;
+  formatLabel?: (value: number, type: string) => string;
+  maxValue?: number;
+  minValue?: number;
+  name?: string;
+  onChange: (value: Range | number) => void;
+  onChangeStart?: (value: Range | number) => void;
+  onChangeComplete?: (value: Range | number) => void;
+  step?: number;
+  value: Range | number;
+}
+
+export default class InputRange extends React.Component<InputRangeProps, any> {
+}
