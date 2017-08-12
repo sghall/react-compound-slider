69ff91a929d3bc00276f4a86eb7c3be29e38c08a
Steve Hall <hall.steven.g@gmail.com>
2017-08-11 17:31:23 -0700

    add video template

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bulma/css/bulma.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
