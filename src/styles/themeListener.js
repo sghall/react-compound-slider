58589bdb24aa6f17ca7154b28f5ffdd022948908
Steve Hall <hall.steven.g@gmail.com>
2017-08-18 16:50:50 -0700

    refactor themeListener

--- src/styles/themeListener.js
+++ src/styles/themeListener.js
@@ -2,27 +2,30 @@
 
 import PropTypes from "prop-types";
 
-export const CHANNEL = "material-ui";
+export const CHANNEL = "react-electric-slide";
 
 const themeListener = {
   contextTypes: {
     [CHANNEL]: PropTypes.object
   },
-  initial: (context: Object) => {
+
+  initial: context => {
     if (!context[CHANNEL]) {
       return null;
     }
 
     return context[CHANNEL].getState();
   },
-  subscribe: (context: Object, cb: Function) => {
+
+  subscribe: (context, cb) => {
     if (!context[CHANNEL]) {
       return null;
     }
 
     return context[CHANNEL].subscribe(cb);
   },
-  unsubscribe(context: Object, subscriptionId: number) {
+
+  unsubscribe(context, subscriptionId) {
     if (context[CHANNEL]) {
       context[CHANNEL].unsubscribe(subscriptionId);
     }
