ddde3f37dc32278043a7d3d4deb8a0bd7a269565
Steve Hall <hall.steven.g@gmail.com>
2017-08-19 10:25:58 -0700

    use babel-eslint/slide deps

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export { default as RangeSlider } from "./components/RangeSlider";
 export { ScaledSlider } from "./components/RangeSlider";
+export { default as Slider } from "./components/Slider";
+export { Range, Handle, createSliderWithTooltip } from "./components/Slider";
