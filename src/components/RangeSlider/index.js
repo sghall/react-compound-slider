851503f2c36e2a3b51e291facf6467b218536493
Steve Hall <hall.steven.g@gmail.com>
2017-08-18 21:34:37 -0700

    add scaled slider

--- src/components/RangeSlider/index.js
+++ src/components/RangeSlider/index.js
@@ -1,1 +1,2 @@
 export { default } from "./RangeSlider";
+export { default as ScaledSlider } from "./ScaledSlider";
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export { default as RangeSlider } from "./components/RangeSlider";
+export { ScaledSlider } from "./components/RangeSlider";
