efead7cc8557a97b70a089ce5fac9721b52922f4
Steve Hall <hall.steven.g@gmail.com>
2017-08-21 14:52:17 -0700

    add precision function

--- src/components/ScaledSlider/utils.js
+++ src/components/ScaledSlider/utils.js
@@ -19,3 +19,15 @@ export function getSliderLength(slider, vertical) {
   const rect = slider.getBoundingClientRect();
   return vertical ? rect.height : rect.width;
 }
+
+export function precision(a) {
+  var match = ("" + num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+  if (!match) {
+    return 0;
+  }
+
+  return Math.max(
+    0,
+    (match[1] ? match[1].length : 0) - (match[2] ? +match[2] : 0)
+  );
+}
