d7d4011bdbb34f5a2215a21075ef9519d98e51fe
Steve Hall <hall.steven.g@gmail.com>
2017-08-22 20:05:41 -0700

    remove edit button

--- docs/src/components/MarkdownDocs.js
+++ docs/src/components/MarkdownDocs.js
@@ -3,7 +3,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "material-ui/styles";
-import Button from "material-ui/Button";
 import MarkdownElement from "docs/src/components/MarkdownElement";
 import Demo from "docs/src/components/Demo";
 
@@ -22,8 +21,7 @@ const headerRegexp = /---\n(.*)\n---/;
 const demoRegexp = /^demo='(.*)'$/;
 const emptyRegexp = /^\s*$/;
 
-const SOURCE_CODE_ROOT_URL =
-  "https://github.com/callemall/material-ui/tree/v1-beta";
+const SOURCE_CODE_ROOT_URL = "https://github.com/sghall";
 
 function MarkdownDocs(props) {
   const { classes, route } = props;
@@ -32,29 +30,8 @@ function MarkdownDocs(props) {
     .split(/^{{|}}$/gm) // Split markdown into an array, separating demos
     .filter(content => !emptyRegexp.test(content)); // Remove empty lines
 
-  let markdownUrl = SOURCE_CODE_ROOT_URL;
-
-  // Map back to the source code
-  if (route.componentAPI) {
-    markdownUrl += `/src${route.componentAPI.path
-      .replace("./component-api/", "/")
-      .replace(".md", ".js")}`;
-  } else if (route.demo) {
-    markdownUrl += `/docs/src/pages/component-demos${route.demo.path.replace(
-      "./",
-      "/"
-    )}`;
-  } else {
-    markdownUrl += `/docs/src/pages${route.path}.md`;
-  }
-
   return (
     <div className={classes.root}>
-      <div className={classes.header}>
-        <Button component="a" href={markdownUrl}>
-          {"Edit this page"}
-        </Button>
-      </div>
       {contents.map(content => {
         if (demoRegexp.test(content)) {
           return <Demo key={content} demo={content.match(demoRegexp)[1]} />;
