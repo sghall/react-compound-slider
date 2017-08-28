fef9cb05d0c224805db58382c9f7c15044071969
Steve Hall <hall.steven.g@gmail.com>
2017-08-27 22:47:32 -0700

    update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,74 @@
 // @flow weak
 
-const path = require('path');
-const webpack = require('webpack');
-const BundleInspector = require('bundle-inspector-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const BundleInspector = require("bundle-inspector-webpack-plugin");
 
-const libraryName = 'resonance';
+const libraryName = "resonance";
 
 const baseConfig = {
   entry: {
-    [libraryName]: path.join(__dirname, 'src/index.js'),
+    [libraryName]: path.join(__dirname, "src/index.js")
   },
   output: {
-    path: path.join(__dirname, 'build/dist'),
+    path: path.join(__dirname, "build/dist"),
     library: libraryName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
+    libraryTarget: "umd",
+    umdNamedDefine: true
   },
   externals: [
     {
       react: {
-        root: 'React',
-        commonjs2: './react',
-        commonjs: ['./react'],
-        amd: 'react',
-      },
-    },
+        root: "React",
+        commonjs2: "./react",
+        commonjs: ["./react"],
+        amd: "react"
+      }
+    }
   ],
   module: {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /(node_modules)/,
         query: {
-          cacheDirectory: true,
-        },
-      },
-    ],
+          cacheDirectory: true
+        }
+      }
+    ]
   },
   resolve: {
-    modules: [
-      path.resolve(__dirname, 'src'),
-      'node_modules',
-    ],
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
   },
-  plugins: [],
+  plugins: []
 };
 
 let config;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   config = Object.assign({}, baseConfig, {
     output: Object.assign({}, baseConfig.output, {
-      filename: `${libraryName}.min.js`,
+      filename: `${libraryName}.min.js`
     }),
     plugins: baseConfig.plugins.concat([
       new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('production'),
+        "process.env.NODE_ENV": JSON.stringify("production")
       }),
       new webpack.optimize.UglifyJsPlugin({
         compress: {
           warnings: false,
-          screw_ie8: true,
-        },
+          screw_ie8: true
+        }
       }),
-      new BundleInspector(),
-    ]),
+      new BundleInspector()
+    ])
   });
 } else {
   config = Object.assign({}, baseConfig, {
     output: Object.assign({}, baseConfig.output, {
-      filename: `${libraryName}.js`,
-    }),
+      filename: `${libraryName}.js`
+    })
   });
 }
 
