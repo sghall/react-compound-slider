648dfe9aa006b50de94205c707e40391c4bb900c
Steve Hall <hall.steven.g@gmail.com>
2017-09-02 09:16:20 -0700

    fix linting

--- src/Links.js
+++ src/Links.js
@@ -1,31 +1,31 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Links extends Component {
   render() {
-    const { scale, values, children } = this.props;
-    const links = [];
+    const { scale, values, children } = this.props
+    const links = []
 
     for (let i = 0; i < values.length + 1; i++) {
-      const s = values[i - 1];
-      const t = values[i];
+      const s = values[i - 1]
+      const t = values[i]
 
       links.push({
-        key: `${s ? s.key : "$"}-${t ? t.key : "$"}`,
+        key: `${s ? s.key : '$'}-${t ? t.key : '$'}`,
         source: s || null,
-        target: t || null
-      });
+        target: t || null,
+      })
     }
 
-    const renderedChildren = children({ links, scale, values });
-    return renderedChildren && React.Children.only(renderedChildren);
+    const renderedChildren = children({ links, scale, values })
+    return renderedChildren && React.Children.only(renderedChildren)
   }
 }
 
 Links.propTypes = {
   scale: PropTypes.func,
   values: PropTypes.array,
-  children: PropTypes.func
-};
+  children: PropTypes.func,
+}
 
-export default Links;
+export default Links
