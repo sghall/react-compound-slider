3ea801560d6c30ae25e52813c35a9ede390446b8
Steve Hall <hall.steven.g@gmail.com>
2017-09-02 09:19:17 -0700

    fix linting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-export { default } from "./Slider";
-export { default as Slider } from "./ComposedSlider";
-export { default as Knobs } from "./Knobs";
-export { default as Links } from "./Links";
-export { default as Ticks } from "./Ticks";
+export { default } from './Slider'
+export { default as Slider } from './ComposedSlider'
+export { default as Knobs } from './Knobs'
+export { default as Links } from './Links'
+export { default as Ticks } from './Ticks'
