4a480e1aa52168ef8b2f9fc37de844d8c7a8ee61
Steve Hall <hall.steven.g@gmail.com>
2017-09-02 11:40:17 -0700

    re-org src

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { default } from './ComposedSlider'
+export { default } from './Slider'
 export { default as Knobs } from './Knobs'
 export { default as Links } from './Links'
 export { default as Ticks } from './Ticks'
