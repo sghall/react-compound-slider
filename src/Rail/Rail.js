87861e44ef86e7161ebd9b7519ae47f67f93e293
Steve Hall <hall.steven.g@gmail.com>
2017-09-06 13:44:32 -0700

    add getRailProps

--- src/Rail/Rail.js
+++ src/Rail/Rail.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { callAll } from '../utils'
 
 class Rail extends Component {
+  getRailProps = (props = {}) => ({
+    ...props,
+    onMouseDown: callAll(props.onMouseDown, this.emitMouse),
+    onTouchStart: callAll(props.onTouchStart, this.emitTouch),
+  })
+
+  emitMouse = e => {
+    this.props.emitMouse(e)
+  }
+
+  emitTouch = e => {
+    this.props.emitTouch(e)
+  }
+
   render() {
     const { children, emitMouse, emitTouch } = this.props
 
-    const renderedChildren = children({ emitMouse, emitTouch })
+    const renderedChildren = children({
+      emitMouse,
+      emitTouch,
+      getRailProps: this.getRailProps,
+    })
     return renderedChildren && React.Children.only(renderedChildren)
   }
 }
