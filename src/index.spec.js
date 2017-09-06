3372b76b01484a10f6b6fe2bf641e1f91ab2075e
Steve Hall <hall.steven.g@gmail.com>
2017-09-05 21:02:12 -0700

    add index spec

---
+++ src/index.spec.js
@@ -0,0 +1,13 @@
+// @flow weak
+/* eslint-env mocha */
+
+/**
+ * Important: This test also serves as a point to
+ * import the entire lib for coverage reporting
+ */
+
+import { assert } from 'chai'
+import * as ReactCompoundSlider from './index'
+
+describe('react-compound-slider', () =>
+  it('should have exports', () => assert.ok(ReactCompoundSlider)))
