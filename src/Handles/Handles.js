efb9543f85592cc24c696323adae53456a3d0374
Steve Hall <hall.steven.g@gmail.com>
2017-09-06 13:44:12 -0700

    add getHandleProps

--- src/Handles/Handles.js
+++ src/Handles/Handles.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { callAll } from '../utils'
 
 class Handles extends Component {
+  getHandleProps = (id, props = {}) => ({
+    ...props,
+    onMouseDown: callAll(props.onMouseDown, e => this.emitMouse(e, id)),
+    onTouchStart: callAll(props.onTouchStart, e => this.emitTouch(e, id)),
+  })
+
+  emitMouse = (e, id) => {
+    this.props.emitMouse(e, id)
+  }
+
+  emitTouch = (e, id) => {
+    this.props.emitTouch(e, id)
+  }
+
   render() {
     const { children, handles, emitMouse, emitTouch } = this.props
 
-    const renderedChildren = children({ handles, emitMouse, emitTouch })
+    const renderedChildren = children({
+      handles,
+      emitMouse,
+      emitTouch,
+      getHandleProps: this.getHandleProps,
+    })
     return renderedChildren && React.Children.only(renderedChildren)
   }
 }
