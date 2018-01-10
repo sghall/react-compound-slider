31c8b5bbef8e69e1962ac51dd99d1cb11d0aaad7
Steve Hall <hall.steven.g@gmail.com>
2018-01-10 12:25:38 -0700

    remove prop from prod build

--- scripts/babel-preset.js
+++ scripts/babel-preset.js
@@ -1,14 +1,19 @@
 // @flow weak
 /* eslint eqeqeq: "off" */
 
-const BABEL_ENV = process.env.BABEL_ENV
-const building =
-  BABEL_ENV != undefined && BABEL_ENV !== 'cjs' && BABEL_ENV !== 'coverage'
+const NE = process.env.NODE_ENV
+const BE = process.env.BABEL_ENV
+
+const building = BE != undefined && BE !== 'cjs' && BE !== 'coverage'
 
 const plugins = []
 
-if (BABEL_ENV === 'umd') {
+if (BE === 'umd') {
   plugins.push('external-helpers')
+
+  if (NE === 'production') {
+    plugins.push('transform-react-remove-prop-types')
+  }
 }
 
 module.exports = {
