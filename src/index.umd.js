607b5bfe3035771ce5f1c51aed5e2db52957db9e
Steve Hall <hall.steven.g@gmail.com>
2018-03-24 20:47:48 -0700

    add umd index with a single default export

---
+++ src/index.umd.js
@@ -0,0 +1,12 @@
+import Slider from './Slider'
+import Rail from './Rail'
+import Ticks from './Ticks'
+import Tracks from './Tracks'
+import Handles from './Handles'
+
+Slider.Rail = Rail
+Slider.Ticks = Ticks
+Slider.Tracks = Tracks
+Slider.Handles = Handles
+
+export default Slider
