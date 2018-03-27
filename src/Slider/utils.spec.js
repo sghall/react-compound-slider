3e0a46291e861120d1a4b9dfc5a63f92d8a37219
Steve Hall <hall.steven.g@gmail.com>
2018-03-26 18:47:00 -0700

    add more tests

--- src/Slider/utils.spec.js
+++ src/Slider/utils.spec.js
@@ -20,4 +20,26 @@ describe('utils', () => {
       assert.notEqual(result, values)
     })
   })
+
+  describe('getSortByVal', () => {
+    it('should return a function', () => {
+      const result = utils.getSortByVal()
+      assert.isFunction(result)
+    })
+
+    it('should correctly sort an array of objects with a val prop', () => {
+      const vals = [
+        { key: 'a', val: 3 },
+        { key: 'b', val: 5 },
+        { key: 'c', val: 3 },
+        { key: 'd', val: 2 },
+      ]
+
+      const result1 = vals.sort(utils.getSortByVal())
+      assert.strictEqual(result1[result1.length - 1].key, 'b')
+
+      const result2 = vals.sort(utils.getSortByVal(true))
+      assert.strictEqual(result2[result2.length - 1].key, 'd')
+    })
+  })
 })
