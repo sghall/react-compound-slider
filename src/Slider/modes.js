a9cd8e3554883a7a43a11550de99ba9cdac4cb00
Steve Hall <hall.steven.g@gmail.com>
2018-03-28 00:41:50 -0700

    new pushable mode

--- src/Slider/modes.js
+++ src/Slider/modes.js
@@ -1,19 +1,70 @@
 // default mode
-export function mode1(currValues, nextValues) {
-  return nextValues
+export function mode1(curr, next) {
+  return next
 }
 
 // prevent duplicate values and crossing
-export function mode2(currValues, nextValues) {
-  for (let i = 0; i < currValues.length; i++) {
-    if (currValues[i].key !== nextValues[i].key) {
-      return currValues
+export function mode2(curr, next) {
+  for (let i = 0; i < curr.length; i++) {
+    if (curr[i].key !== next[i].key) {
+      return curr
     }
 
-    if (nextValues[i + 1] && nextValues[i].val === nextValues[i + 1].val) {
-      return currValues
+    if (next[i + 1] && next[i].val === next[i + 1].val) {
+      return curr
     }
   }
 
-  return nextValues
+  return next
+}
+
+// pushable mode
+export function mode3(curr, next, step, getValue) {
+  let indexForMovingHandle = -1
+  let handleMoveIsPositive = true
+
+  for (let i = 0; i < curr.length; i++) {
+    const c = curr[i]
+    const n = next[i]
+
+    // make sure keys are in same order if not return curr
+    if (!n || n.key !== c.key) {
+      return curr
+    } else if (n.val !== c.val) {
+      indexForMovingHandle = i
+      handleMoveIsPositive = n.val - c.val > 0
+    }
+  }
+
+  // nothing has changed (shouldn't happen but just in case).
+  if (indexForMovingHandle === -1) {
+    return curr
+  } else {
+    const increment = handleMoveIsPositive ? step : -step
+
+    for (let i = 0; i < next.length; i++) {
+      const n0 = next[i]
+      const n1 = next[i + 1]
+
+      if (n1 && n0.val === n1.val) {
+        if (i === indexForMovingHandle) {
+          const newStep = n1.val + increment
+          if (getValue(newStep) !== newStep) {
+            return curr
+          } else {
+            n1.val = n1.val + increment
+          }
+        } else {
+          const newStep = n0.val + increment
+          if (getValue(newStep) !== newStep) {
+            return curr
+          } else {
+            n0.val = n0.val + increment
+          }
+        }
+      }
+    }
+  }
+
+  return next
 }
