4886d10bacd532224583be18de2ecf65f9fc00a4
Steve Hall <hall.steven.g@gmail.com>
2018-08-12 16:17:09 -0700

    copy types on build

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -16,6 +16,18 @@ function copyFile(file) {
   }).then(() => console.log(`Copied ${file} to ${buildPath}`))
 }
 
+function copyTypes(src, dst) {
+  const buildPath = path.resolve(__dirname, '../build/', dst)
+  return new Promise(resolve => {
+    fse.copy(src, buildPath, err => {
+      if (err) {
+        throw err
+      }
+      resolve()
+    })
+  }).then(() => console.log(`Copied ${src} to ${buildPath}`))
+}
+
 function createPackageFile() {
   return new Promise(resolve => {
     fse.readFile(
@@ -53,6 +65,7 @@ function createPackageFile() {
         main: './index.js',
         module: './index.es.js',
         'jsnext:main': './index.es.js',
+        types: './index.d.ts',
         keywords,
         repository,
         license,
@@ -78,4 +91,20 @@ function createPackageFile() {
 
 const files = ['README.md', 'LICENSE']
 
-Promise.all(files.map(file => copyFile(file))).then(() => createPackageFile())
+const types = [
+  ['src/index.d.ts', 'index.d.ts'],
+  ['src/Slider/index.d.ts', 'Slider/index.d.ts'],
+  ['src/Slider/Slider.d.ts', 'Slider/Slider.d.ts'],
+  ['src/Rail/index.d.ts', 'Rail/index.d.ts'],
+  ['src/Rail/Rail.d.ts', 'Rail/Rail.d.ts'],
+  ['src/Handles/index.d.ts', 'Handles/index.d.ts'],
+  ['src/Handles/Handles.d.ts', 'Handles/Handles.d.ts'],
+  ['src/Tracks/index.d.ts', 'Tracks/index.d.ts'],
+  ['src/Tracks/Tracks.d.ts', 'Tracks/Tracks.d.ts'],
+  ['src/Ticks/index.d.ts', 'Ticks/index.d.ts'],
+  ['src/Ticks/Ticks.d.ts', 'Ticks/Ticks.d.ts'],
+]
+
+Promise.all(files.map(file => copyFile(file)))
+  .then(() => Promise.all(types.map(file => copyTypes(...file))))
+  .then(() => createPackageFile())
