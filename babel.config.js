1cb6e4bbe8a27304e4addd07dfa6af55c132982a
Steve Hall <hall.steven.g@gmail.com>
2019-01-14 23:00:02 -0700

    fix test coverage

--- babel.config.js
+++ babel.config.js
@@ -13,5 +13,21 @@ module.exports = {
       ],
       plugins: ['@babel/plugin-proposal-class-properties'],
     },
+    coverage: {
+      exclude: /node_modules/,
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: 'commonjs',
+          },
+        ],
+        '@babel/preset-react',
+      ],
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+        'babel-plugin-istanbul',
+      ],
+    },
   },
 }
