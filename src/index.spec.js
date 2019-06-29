19edb13ab32a48305294438bd5347b87978e171f
Steve Hall <hall.steven.g@gmail.com>
2019-06-29 12:37:32 -0700

    add test for mode export

--- src/index.spec.js
+++ src/index.spec.js
@@ -7,7 +7,20 @@
  */
 
 import { assert } from 'chai'
-import * as ReactCompoundSlider from './index'
+import Slider, { mode1, mode2, mode3 } from './index'
 
-describe('react-compound-slider', () =>
-  it('should have exports', () => assert.ok(ReactCompoundSlider)))
+describe('react-compound-slider', () => {
+  it('should have default export', () => assert.ok(Slider))
+  it('should export mode1', () => {
+    assert.ok(mode1)
+    assert.typeOf(mode1, 'function')
+  })
+  it('should export mode2', () => {
+    assert.ok(mode2)
+    assert.typeOf(mode2, 'function')
+  })
+  it('should export mode3', () => {
+    assert.ok(mode3)
+    assert.typeOf(mode3, 'function')
+  })
+})
