836a84897d10f1fb8779eedfb4aac85afb23a0b1
Steve Hall <hall.steven.g@gmail.com>
2020-10-27 20:24:18 -0700

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -63,7 +63,6 @@ export default [
             extensions,
           }),
           terser({
-            sourcemap: true,
             output: { comments: false },
             compress: {
               keep_infinity: true,
