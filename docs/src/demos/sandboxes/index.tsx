28ecd8910a274295b7b4d6639049f4e022076db0
Steven Hall <sghall@protonmail.com>
2020-11-13 20:49:07 -0700

    update docs

--- docs/src/demos/sandboxes/index.tsx
+++ docs/src/demos/sandboxes/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-import { Example1 } from './Example1';
+// import { Example1 } from './Example1';
 import { Example2 } from './Example2';
 import { Example3 } from './Example3';
 
 export default () => (
   <Grid spacing={2} container direction="column">
-    <Grid>
+    {/* <Grid>
       <Typography variant="h6">Video Slider</Typography>
       <Example1 />
-    </Grid>
+    </Grid> */}
     <Grid>
       <Typography variant="h6">Date/Time Slider</Typography>
       <Example2 />
