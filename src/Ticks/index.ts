40f0acfcecc4eba0068e70f61def442a2d028900
David Markov <Markov.David@seznam.cz>
2022-08-20 15:26:01 +0200

    feat: clickable ticks

--- src/Ticks/index.ts
+++ src/Ticks/index.ts
@@ -1,9 +1,11 @@
 import {
   TicksProps as _TicksProps,
   TicksObject as _TicksObject,
+  GetTickProps as _GetTickProps
 } from './types';
 
 export { Ticks } from './Ticks';
 
 export type TicksProps = _TicksProps;
 export type TicksObject = _TicksObject;
+export type GetTickProps = _GetTickProps
